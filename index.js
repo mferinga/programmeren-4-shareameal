90e2a653ed44848905bed833b49db989f72ca667
Matthijs Feringa <mt.feringa@student.avans.nl>
2022-04-25 22:19:21 +0200

    put and deleted are working as intended

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 let database = [];
+let userDatabase = [];
 let id = 0;
 
 app.all("*", (req, res, next) => {
@@ -47,8 +48,8 @@ app.get("/api/movie/:movieId", (req, res, next) => {
       result: movie,
     });
   } else {
-    res.status(401).json({
-      status: 401,
+    res.status(404).json({
+      status: 404,
       result: `Movie with ID ${movieId} not found`,
     });
   }
@@ -61,9 +62,98 @@ app.get("/api/movie", (req, res, next) => {
   });
 });
 
+app.get("/api/user", (req, res) => {
+  res.status(300).json({
+    status: 300,
+    result: userDatabase,
+  });
+});
+
+app.post("/api/user", (req, res) => {
+  let user = req.body;
+  user = {
+    id,
+    ...user,
+  };
+  id++;
+  console.log(user);
+  userDatabase.push(user);
+  res.status(301).json({
+    status: 301,
+    result: userDatabase,
+  });
+});
+
+app.get("/api/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  console.log(`User met ID ${userId} gezocht`);
+  let user = userDatabase.filter((item) => item.id == userId);
+  if(user.length > 0){
+    console.log(user);
+    res.status(300).json({
+      status : 300,
+      result: user,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `User with Id ${userId} is not found`
+    });
+  }
+});
+
+app.put("/api/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  console.log(`User met ID ${userId} gezocht`);
+  let oldUser = userDatabase.filter((item) => item.id == userId);
+  if(oldUser.length > 0){
+    console.log(oldUser);
+    elementIndex = userDatabase.findIndex((obj => obj.id == userId));
+    
+    let user = req.body;
+    newUser = {
+      id,
+      ...user,
+    } 
+    userDatabase[elementIndex] = newUser;
+
+    res.status(302).json({
+      status: 302,
+      result: userDatabase,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `User with Id ${userId} is not found`
+    });
+  }
+});
+
+app.delete("/api/user/:userId", (req, res) => {
+  const userId = req.params.userId;
+  console.log(`User met ID ${userId} gezocht`);
+  let user = userDatabase.filter((item) => item.id == userId);
+  if(user.length > 0){
+    console.log(userDatabase[userId]);
+
+    const index = userDatabase.indexOf(user);
+    userDatabase.splice(index, 1);
+    res.status(303).json({
+      status: 303,
+      result: userDatabase,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `User with Id ${userId} is not found`
+    });
+  }
+});
+
 app.all("*", (req, res) => {
-  res.status(401).json({
-    status: 401,
+  res.status(404).json({
+    status: 404,
     result: "End-point not found",
   });
 });
@@ -71,3 +161,5 @@ app.all("*", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
