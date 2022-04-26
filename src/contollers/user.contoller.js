4476cb16baf540a00298dd943e0ce946ffad7c46
Matthijs Feringa <mt.feringa@student.avans.nl>
2022-04-26 11:28:27 +0200

    Assert added

--- src/contollers/user.contoller.js
+++ src/contollers/user.contoller.js
@@ -1,7 +1,26 @@
+const assert = require('assert');
 let userDatabase = [];
 let id = 0;
 
 let controller = {
+    validateUser:(req, res, next)=>{
+        console.log("i am in the validation")
+        let user = req.body;
+        let{name, email, age, password } = user
+        try{
+            assert(typeof name === 'string', 'Name must be a string');
+            assert(typeof email === 'string', 'Email must be a string');
+            assert(typeof age === 'number', 'Age must be a number');
+            assert(typeof password === 'string', 'Password must be a string');
+            next()
+        } catch(error){
+            console.log(error)
+            res.status(400).json({
+                status: 400,
+                result: error.toString(),
+            })
+        }
+    },
     addUser:(req, res)=>{
         let user = req.body;
         user = {
@@ -39,6 +58,53 @@ let controller = {
             });
         }
     },
+    updateUserById:(req, res) =>{
+        const userId = req.params.userId;
+
+        console.log(`User met ID ${userId} gezocht`);
+        let oldUser = userDatabase.filter((item) => item.id == userId);
+        if(oldUser.length > 0){
+            console.log(oldUser);
+            elementIndex = userDatabase.findIndex((obj => obj.id == userId));
+            
+            let user = req.body;
+            newUser = {
+                id,
+            ...user,
+            } 
+            userDatabase[elementIndex] = newUser;
+
+            res.status(205).json({
+            status: 205,
+            result: userDatabase,
+            });
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: `User with Id ${userId} is not found`
+            });
+        }
+    },
+    deleteUserById:(req, res) =>{
+        const userId = req.params.userId;
+        console.log(`User met ID ${userId} gezocht`);
+        let user = userDatabase.filter((item) => item.id == userId);
+        if(user.length > 0){
+            console.log(userDatabase[userId]);
+
+            const index = userDatabase.indexOf(user);
+            userDatabase.splice(index, 1);
+            res.status(206).json({
+                status: 206,
+                result: userDatabase,
+            });
+        } else {
+            res.status(401).json({
+                status: 401,
+                result: `User with Id ${userId} is not found`
+            });
+        }
+    }
 }
 
 module.exports = controller;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,64 +1,18 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
 const userController = require('../contollers/user.contoller')
 
 let userDatabase = [];
 let id = 0;
 
-router.get("/api/user", userController.getAllUsers)
+router.get("/api/user", userController.getAllUsers);
 
-router.post("/api/user", userController.addUser);
+router.post("/api/user",userController.validateUser, userController.addUser);
 
-router.get("/api/user/:userId", userController.getUserById)
+router.get("/api/user/:userId", userController.getUserById);
 
-app.put("/api/user/:userId", (req, res) => {
-    const userId = req.params.userId;
+router.put("/api/user/:userId", userController.updateUserById);
 
-    console.log(`User met ID ${userId} gezocht`);
-    let oldUser = userDatabase.filter((item) => item.id == userId);
-    if(oldUser.length > 0){
-        console.log(oldUser);
-        elementIndex = userDatabase.findIndex((obj => obj.id == userId));
-        
-        let user = req.body;
-        newUser = {
-            id,
-         ...user,
-        } 
-        userDatabase[elementIndex] = newUser;
-
-        res.status(205).json({
-        status: 205,
-        result: userDatabase,
-        });
-    } else {
-        res.status(401).json({
-            status: 401,
-            result: `User with Id ${userId} is not found`
-        });
-    }
-});
-
-app.delete("/api/user/:userId", (req, res) => {
-    const userId = req.params.userId;
-    console.log(`User met ID ${userId} gezocht`);
-    let user = userDatabase.filter((item) => item.id == userId);
-    if(user.length > 0){
-        console.log(userDatabase[userId]);
-
-        const index = userDatabase.indexOf(user);
-        userDatabase.splice(index, 1);
-        res.status(206).json({
-            status: 206,
-            result: userDatabase,
-        });
-    } else {
-        res.status(401).json({
-            status: 401,
-            result: `User with Id ${userId} is not found`
-        });
-    }
-});
+router.delete("/api/user/:userId", userController.deleteUserById)
 
 module.exports = router;
